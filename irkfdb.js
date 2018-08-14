8a3b6aa37ba0abafea43ae39fd9ec6c0ee79ffe4
Swapnil Sarwe <swapnilsarwe@gmail.com>
2018-08-14 14:35:16 +0530

    Update irkfdb.js
    moved to https

--- irkfdb.js
+++ irkfdb.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var VERSION_NUMBER = 1.0
         API_CLIENT = 'CHROME_EXT';
     function getRandomQuote() {
-        $.get("http://api.irkfdb.in/facts/random?api_type=" + API_CLIENT + "&version_number=" + VERSION_NUMBER, function (data) {
+        $.get("https://api.irkfdb.in/facts/random?api_type=" + API_CLIENT + "&version_number=" + VERSION_NUMBER, function (data) {
             $("#fact").html(data.resultSet.data[0].fact);
             setMetaInfo('categories', 'Categories', data.resultSet.data[0].categories);
             setMetaInfo('sources', 'Sources', data.resultSet.data[0].sources);
